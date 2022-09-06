d40c630145144f9d0a22dc0aa9fcec333ea8b45d
kongruizeng <1627348835@qq.com>
2022-09-06 21:07:05 +0800

    feat:login登陆样式完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="login">登陆</div>
-    <div class="box"></div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -15,15 +9,9 @@ export default {}
 </script>
 
 <style scoped>
-.box {
-  width: 2rem;
-  height: 2rem;
-  background-color: gold;
-}
 .login {
   width: 700px;
-  height: 200px;
-  background-color: skyblue;
-  margin: auto;
+  height: 88px;
+  background: skyblue;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 引入css
+import '@/assets/fonts/iconfont.css'
+// 引入vant
 import Vant from 'vant'
 import 'vant/lib/index.css'
-import 'amfe-flexible'
 
+// 引入flexible
+import 'amfe-flexible'
 Vue.use(Vant)
 
 Vue.config.productionTip = false
