93ed182b4248962548b0351152eb1823ebe290d7
kongruizeng <1627348835@qq.com>
2022-09-06 16:13:56 +0800

    feat:postcss配置完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>黑马头条</div>
+  <div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <div class="login">登陆</div>
+    <div class="box"></div>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style scoped>
+.box {
+  width: 2rem;
+  height: 2rem;
+  background-color: gold;
+}
+.login {
+  width: 700px;
+  height: 200px;
+  background-color: skyblue;
+  margin: auto;
+}
+</style>
