c2d5cfa70d345600b2822e022a8e636a30b13699
kongruizeng <1627348835@qq.com>
2022-09-07 17:19:24 +0800

    feat:登陆页面完成

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -9,19 +9,12 @@
     <!-- name:表示表单项 -->
     <!-- rules是一个数组 -->
     <!-- rule:{ required:是否必选,message:错误的提示信息,pattern:正则,trigger:规则的触发时机onChange、onBlur } -->
-    <van-form @submit="onSubmit" class="form">
+    <van-form @submit="onSubmit" class="form" ref="form">
       <van-field
         v-model="mobile"
         name="mobile"
         placeholder="请输入手机号"
-        :rules="[
-          { required: true, message: '请填写手机号' },
-          {
-            pattern:
-              /^(0|86|17951)?(13[0-9]|15[012356789]|166|17[3678]|18[0-9]|14[57])[0-9]{8}$/,
-            message: '手机格式错误'
-          }
-        ]"
+        :rules="mobileRules"
       >
         <template #label>
           <span class="toutiao toutiao-shouji"></span>
@@ -32,13 +25,31 @@
         v-model="code"
         name="code"
         placeholder="请输入验证码"
-        :rules="[
-          { required: true, message: '请输入验证码' },
-          { pattern: /[0-9]{6}/, message: '验证码格式错误' }
-        ]"
+        :rules="codeRules"
       >
+        <!-- 字体图标 -->
         <template #label>
           <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+        <!-- 验证码 -->
+        <template #button>
+          <van-button
+            class="btn"
+            block
+            type="default"
+            size="small"
+            round
+            native-type="button"
+            v-if="isShowCodeBtn"
+            @click="sendCode"
+            >获取验证码</van-button
+          >
+          <van-count-down
+            v-else
+            :time="3 * 1000"
+            format="ss秒"
+            @finish="isShowCodeBtn = true"
+          />
         </template>
       </van-field>
       <div style="margin: 16px">
@@ -49,16 +60,63 @@
 </template>
 
 <script>
+import { mobileRules, codeRules } from './rule'
+import { login, sendCodeAPI } from '@/api'
+import { mapMutations } from 'vuex'
 export default {
   data() {
     return {
       mobile: '',
-      code: ''
+      code: '',
+      codeRules,
+      mobileRules,
+      isShowCodeBtn: true
     }
   },
   methods: {
-    onSubmit(values) {
-      console.log('submit', values)
+    ...mapMutations(['SET_TOKEN']),
+    loading() {
+      this.$toast.loading({
+        message: '加载中...',
+        forbidClick: true,
+        duration: 0
+      })
+    },
+    async onSubmit() {
+      this.loading()
+      try {
+        const { data } = await login(this.mobile, this.code)
+        this.SET_TOKEN(data.data)
+        this.$router.push('/profile')
+        this.$toast.success('登陆成功')
+      } catch (error) {
+        if (error.response && error.response.status === 400) {
+          this.$toast.fail(error.response.data.message)
+        } else {
+          // console.dir(error)
+          this.$toast.clear()
+          throw error
+        }
+      }
+    },
+    async sendCode() {
+      await this.$refs.form.validate('mobile')
+      this.loading()
+      try {
+        await sendCodeAPI(this.mobile)
+        this.isShowCodeBtn = false
+        this.$toast.success('发送验证码成功')
+      } catch (error) {
+        if (
+          error.response &&
+          (error.response.status === 429 || error.response.status === 404)
+        ) {
+          this.$toast.fail(error.response.data.message)
+        } else {
+          this.$toast.clear()
+          throw error
+        }
+      }
     }
   }
 }
@@ -86,5 +144,10 @@ export default {
   .toutiao {
     font-size: 40px;
   }
+}
+.btn {
+  height: 0.64rem;
+  background-color: #eee;
+  color: #a58594;
 }
 </style>
